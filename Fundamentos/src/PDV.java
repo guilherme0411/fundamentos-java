dd2f0be8d1ee1bba552da67d28f6e6351d5ef5d6
Guilherme Oliveira <44686070+guilherme0411@users.noreply.github.com>
2019-06-05 21:46:05 -0300

    Upload do PDV

---
+++ Fundamentos/src/PDV.java
@@ -0,0 +1,46 @@
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author guilherme.oliveira17
+ *
+ */
+public class PDV {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double valor, porcentagem, desconto = 0, calculo, total, pago, troco;
+		Date data = new Date();
+		
+		DateFormat Formatador = 
+		DateFormat.getDateInstance(DateFormat.FULL);
+		System.out.println(Formatador.format(data));
+		Scanner leia = new Scanner(System.in);
+		System.out.println("===============");
+		System.out.println("======= PDV =======");
+		System.out.println("===============");
+		System.out.println("");
+		System.out.println("Valor total: ");
+		valor = leia.nextDouble();
+		System.out.println("Desconto em %: ");
+		porcentagem = (valor * desconto) / 100;  
+		desconto = leia.nextDouble();
+		calculo = (desconto * valor) / 100 ;
+		total = (valor - calculo);
+		System.out.println("Total com desconto: " + total);
+		System.out.println("_____________________________");
+		System.out.println("");
+		System.out.println("Valor pago: ");
+		pago = leia.nextDouble();
+		troco = (pago - total);
+		System.out.println("Troco: " + troco);
+	}
+
+}
